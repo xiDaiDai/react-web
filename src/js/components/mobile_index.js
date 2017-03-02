68e1e6596a19c17dd686a74f6836ef32ce26d7ec
xiDaiDai <xishiwei@sina.cn>
2017-03-02 21:17:57 +0800

    add footer component

--- src/js/components/mobile_index.js
+++ src/js/components/mobile_index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MobileHeader from './mobile_header';
+import MobileFooter from './mobile_footer';
 
 export default class MobileIndex extends React.Component {
 	
 	render() {
 		return (
-			 <MobileHeader></MobileHeader>
+			<div>
+			 	<MobileHeader></MobileHeader>
+			 	<MobileFooter></MobileFooter>
+			</div>
 		);
 	};
 }
--- src/js/components/pc_index.js
+++ src/js/components/pc_index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
+
 
 export default class PCIndex extends React.Component {
 	
 	render() {
 		return (
-			 <PCHeader></PCHeader>
+			<div>
+			  <PCHeader></PCHeader>
+				<PCFooter></PCFooter>
+			</div>
 		);
 	};
 }
