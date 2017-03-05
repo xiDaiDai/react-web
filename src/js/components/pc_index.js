92a49061fdffa85e4d0818a26d55da8f434c1e15
xiDaiDai <xishiwei@sina.cn>
2017-03-06 00:09:51 +0800

    add pc carousel&news block

--- src/js/components/pc_index.js
+++ src/js/components/pc_index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
-
+import PCNewsContainer from './pc_newscontainer';
 
 export default class PCIndex extends React.Component {
 	
@@ -9,6 +9,7 @@ export default class PCIndex extends React.Component {
 		return (
 			<div>
 			  <PCHeader></PCHeader>
+			  <PCNewsContainer></PCNewsContainer>
 				<PCFooter></PCFooter>
 			</div>
 		);
