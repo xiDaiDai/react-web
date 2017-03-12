f8132630bfb02382174ed73be3e87afd43f4cf38
xiDaiDai <xishiwei@sina.cn>
2017-03-12 23:03:24 +0800

    add collection

--- src/js/components/common_comments.js
+++ src/js/components/common_comments.js
@@ -6,7 +6,8 @@ import {
 	Col,
 	Button,
 	Icon,
-	Card
+	Card,
+	notification
 } from 'antd';
 import 'antd/dist/antd.css';
 const FormItem = Form.Item;
@@ -29,13 +30,22 @@ class CommonComments extends React.Component {
 
 	handleSubmit(e) {
 		e.preventDefault();
-		var myFetchOptions = {	
+		let myFetchOptions = {	
 			method: 'GET'
 		};
-		var formdata = this.props.form.getFieldsValue();
+		let formdata = this.props.form.getFieldsValue();
 		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark, myFetchOptions).then(response => response.json()).then(json => {
 			this.componentDidMount();
 		})
+	};
+
+	 addUserCollection(){
+  	let myFetchOptions = {
+			method: 'GET'
+		};
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+			notification['success']({message: 'ReactNews提醒', description: '收藏此文章成功'});
+		});
 	};
 
 	render() {
@@ -59,6 +69,8 @@ class CommonComments extends React.Component {
           		)}
         			</FormItem>
        			 	<Button type="primary" htmlType="submit">提交评论</Button>
+       			 	&nbsp;&nbsp;
+							<Button type="primary" htmlType="button" onClick={()=>this.addUserCollection()}>收藏该文章</Button>
 						</Form>
 					</Col>
 				</Row>
